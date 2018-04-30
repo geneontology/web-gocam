7084dbaef1b81aa3d11c616eea88ae7188935c30
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2018-04-30 10:54:20 -0700

    Making the site responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { UserService, UsageType } from './user.service';
 import { PreferencesService } from './preferences.service';
 
@@ -7,7 +7,7 @@ import { PreferencesService } from './preferences.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
 
   usages: string[];
@@ -22,6 +22,10 @@ export class AppComponent {
       UsageType.curation,
       UsageType.development
     ];
+    this.autoSideNavState(window.innerWidth);
+  }
+
+  ngOnInit(): void {
   }
 
   isUsageDefined(): boolean {
@@ -37,4 +41,16 @@ export class AppComponent {
     return this._prefs.isSideNavShown();
   }
 
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.autoSideNavState(event.target.innerWidth);
+  }
+
+  autoSideNavState(width: number) {
+//    if(width < 1000)
+//    this._prefs.setSideNavState(false);
+    this._prefs.setSideNavState(width > 1000);
+  }
+    
 }
