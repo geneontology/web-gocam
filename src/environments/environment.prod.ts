e330694063617dfe9ff60df9ee84a50ac0d78d2c
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2018-07-20 15:29:30 -0700

    Correct Swagger URL to point only to the last version of the API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   rdfStore : "http://rdf.geneontology.org/blazegraph/sparql",
   apiUrl : "https://api.geneontology.cloud/",
-  swaggerUrl : "https://app.swaggerhub.com/apis/geneontology/gosparql/2.01.0"
+  swaggerUrl : "https://app.swaggerhub.com/apis/geneontology/gosparql"
 };
