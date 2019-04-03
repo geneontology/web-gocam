c65f28d2b1d9543c948447c2d6c7fd309f77c8d7
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-04-03 04:01:13 -0700

    Slight style update

--- src/app/documentation/introduction/introduction.component.ts
+++ src/app/documentation/introduction/introduction.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { CytoscapeService } from '../../core/cytoscape-service';
+import { UrlHandlerService } from '../../core/url-handler.service';
+import { DownloadType } from '../../downloads/downloads.component';
 
 @Component({
   selector: 'app-introduction',
@@ -12,7 +14,8 @@ export class IntroductionComponent implements OnInit {
 
   zoomEnabled = true;
 
-  constructor(private cytoService: CytoscapeService) {
+  constructor(public urlHandler: UrlHandlerService,
+              private cytoService: CytoscapeService) {
     this.cytoscape = cytoService.getCytoscape();
   }
 
@@ -26,7 +29,22 @@ export class IntroductionComponent implements OnInit {
 
   }
 
-
+  download(type: DownloadType): void {
+    switch(type) {
+      case DownloadType.ttl: 
+      window.location.href = this.urlHandler.downloadTTL();
+      break;
+      case DownloadType.jnl:
+      window.location.href = this.urlHandler.downloadJNL();
+      break;
+      case DownloadType.sif:
+      window.location.href = this.urlHandler.downloadSIF();
+      break;
+      case DownloadType.ctab:
+      window.location.href = this.urlHandler.downloadCTAB();
+      break;
+    }
+  }
 
 }
 
